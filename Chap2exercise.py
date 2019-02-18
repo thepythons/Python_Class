b5d9ec0932bfd2566c53b7353274a54041443f94
anand004 <gaurav.anand004@gmail.com>
2019-02-18 22:08:15 +0530

    Chap2

---
+++ Chap2exercise.py
@@ -0,0 +1,30 @@
+#exercise2
+name=input("Enter your name: ")
+print("Hello {}".format(name))
+
+#exercise3
+hours=int(input("Enter Hour: "))
+rate=float(input("Enter Rate: "))
+pay=hours*rate
+print("Pay:",pay)
+
+#exercise4
+width=17
+height=12.0
+value1=width//2
+print(value1)
+print(type(value1))
+value2=width/2.0
+print(value2)
+print(type(value2))
+value3=height/3
+print(value3)
+print(type(value3))
+value4= 1+2*5
+print(value4)
+print(type(value4))
+
+#exercise5
+cel_temp=int(input("Enter temperature in celsius"))
+far_temp=(cel_temp*9/5)+32
+print(far_temp)
